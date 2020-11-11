bae707655df266097a463b5e6514855cafdf07d7
Dmitry Romashov <framebassman@gmail.com>
2020-11-11 15:01:20 +0300

    sonar fixes

--- src/test/java/ru/crystals/testingtools/sql/ConnectionPool.java
+++ src/test/java/ru/crystals/testingtools/sql/ConnectionPool.java
@@ -2,19 +2,20 @@ package ru.crystals.testingtools.sql;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.ArrayList;
 import java.util.Vector;
 
 public class ConnectionPool {
     private static ConnectionPool instance;
-    private Vector<Connection> availableConns = new Vector<>();
-    private Vector<Connection> usedConns = new Vector<>();
+    private ArrayList<Connection> availableConns = new ArrayList<>();
+    private ArrayList<Connection> usedConns = new ArrayList<>();
     private String url;
     private String connectionString = "jdbc:postgresql://%s:%s/%s?user=%s&password=%s";
 
     private ConnectionPool(String address, String base, String port, String username, String password, int initConnCnt) {
         url = String.format(connectionString, address, port, base, username, password);
         for (int i = 0; i < initConnCnt; i++) {
-            availableConns.addElement(getConnection());
+            availableConns.add(getConnection());
         }
     }
 
@@ -42,17 +43,17 @@ public class ConnectionPool {
             newConn = getConnection();
         } else {
             System.out.println(Thread.currentThread().getId() + ": get last available");
-            newConn = availableConns.lastElement();
-            availableConns.removeElement(newConn);
+            newConn = availableConns.get(availableConns.size() - 1);
+            availableConns.remove(newConn);
         }
-        usedConns.addElement(newConn);
+        usedConns.add(newConn);
         return newConn;
     }
 
     public synchronized void putback(Connection c) throws NullPointerException {
         if (c != null) {
-            if (usedConns.removeElement(c)) {
-                availableConns.addElement(c);
+            if (usedConns.remove(c)) {
+                availableConns.add(c);
             } else {
                 throw new NullPointerException("Connection not in the usedConns array");
             }
